6730072db3a2d2b26c7bda6b8527b3c4193f9853
reggae-krk <monika.gorecka.agh@gmail.com>
2018-09-13 11:48:25 +0200

    Create quickSort method

--- src/main/java/QuickSort.java
+++ src/main/java/QuickSort.java
@@ -5,4 +5,19 @@ public class QuickSort {
         int n = array.length;
         quickSort(array, 0, n - 1);
     }
+
+    private static void quickSort(int[] array, int left, int right) {
+        if(left >= right) return;
+
+        int border = partitionArray(array, left, right);
+
+        if(border - left < right - border) {
+            quickSort(array, left, border - 1);
+            quickSort(array, border + 1, right);
+        }
+        else {
+            quickSort(array, border + 1, right);
+            quickSort(array, left, border - 1);
+        }
+    }
 }
